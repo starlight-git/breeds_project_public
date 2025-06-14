2b2f4ec481142ec7b649e05ba0afc425ee772dfd
starlight-git <nuiswa22@gmail.com>
2025-06-14 18:47:53 -0400

    Delete breeds_project_public directory

--- breeds_project_public/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="w-full bg-gray-100 text-gray-600 py-6"
-        >
-            This is a footer. 
-        </footer>
-    )
-}
--- breeds_project_public/lib/prismaClient.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const globalForprisma = global as unknown as { prisma: PrismaClient};
-
-export const prisma = globalForprisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForprisma.prisma = prisma;
