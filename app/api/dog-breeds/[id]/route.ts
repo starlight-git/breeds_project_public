701159cfde88edfe96d24df6f1f66a28b59a194e
starlight-git <nuiswa22@gmail.com>
2025-06-14 21:12:56 -0400

    Update route.ts

--- app/api/dog-breeds/[id]/route.ts
+++ app/api/dog-breeds/[id]/route.ts
@@ -1,18 +1,21 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import * as DogBreedService from '@/services/dogBreedsService';
-import {DogBreedsById} from "@/types/DogBreed";
+import { DogBreedsById } from '@/types/DogBreed';
 
-export async function GET(_: Request, { params }: { params: { id: string } }) {
-    const { id } = await params;
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = params;
+
     if (!id) {
         return NextResponse.json({ message: 'ID is required' }, { status: 400 });
     }
 
     try {
         const breed: DogBreedsById = await DogBreedService.fetchDogBreedById(id);
+
         if (!breed) {
             return NextResponse.json({ message: 'Breed not found' }, { status: 404 });
         }
+
         return NextResponse.json(breed);
     } catch (err) {
         console.error('❌ Failed to fetch dog breed by ID:', err);
